15a248054d1b9852b0bfa85b7473ad77f2de9d08
Matthew Maynes <matthew.maynes@yahoo.ca>
2017-02-09 11:48:20 -0500

    Fixed JSON path

--- src/main/java/buddy/AddressController.java
+++ src/main/java/buddy/AddressController.java
@@ -2,10 +2,7 @@ package buddy;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
--- src/test/java/buddy/AddressControllerTest.java
+++ src/test/java/buddy/AddressControllerTest.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 
 @RunWith(SpringRunner.class)
@@ -24,15 +26,12 @@ public class AddressControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private AddressController service;
-
     @Test
     public void validateAdd() throws Exception {
         mockMvc.perform(post("/add?name=Tom&phone=111"))
-                .andExpect(status().isOk());
-               // .andExpect(jsonPath("$.name").value("Tom"))
-               // .andExpect(jsonPath("$.phoneNumber").value("111"));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Tom"))
+                .andExpect(jsonPath("$.phoneNumber").value("111"));
 
     }
 
